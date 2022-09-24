35fac6eabb390f233c2706545f080c0749b86495
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-09-23 17:40:37 -0700

    added links to the images

--- public/script.js
+++ public/script.js
@@ -21,6 +21,8 @@ async function getCurr() {
 }
 
 
+
+
 let currStore = new Array();
 
 function sortImage(val) {
@@ -33,8 +35,36 @@ function sortImage(val) {
 }
 
 let boxes = 6;
-let currBox1 = 0;
-let pageIndex = 6;
+let currPage = 1;
+//if next page is clicked add 6 to the pageIndex
+let pageIndex = 0;
+//Total pages is set in the load function to count how many pages can be loaded
+let totalPages;
+let extra;
+
+
+function setPage() {
+    for(let i = 0; i < 6; i++) {
+        if(i == 0) {
+            box1.style.backgroundImage = `url(${currStore[0 + pageIndex].image})`
+        }
+        if(i == 1) {
+            box2.style.backgroundImage = `url(${currStore[1 + pageIndex].image})`
+        }
+        if(i == 2) {
+            box3.style.backgroundImage = `url(${currStore[2 + pageIndex].image})`
+        }
+        if(i == 3) {
+            box4.style.backgroundImage = `url(${currStore[3 + pageIndex].image})`
+        }
+        if(i == 4) {
+            box5.style.backgroundImage = `url(${currStore[4 + pageIndex].image})`
+        }
+        if(i == 5) {
+            box6.style.backgroundImage = `url(${currStore[5 + pageIndex].image})`
+        }
+    }
+}
 
 
 async function loadPage() {
@@ -43,33 +73,48 @@ async function loadPage() {
     for(let i = 0; i < val.news.length; i++) {
         if(val.news[i].image != "None"){
             currStore[currStore.length] = val.news[i];
-            console.log(i)
+            //console.log(i)
         }
     }
     console.log(currStore);
-    let extra = currStore.length % boxes;
-    let totalPages = currStore.length - extra;
+    extra = currStore.length % boxes;
+    totalPages = currStore.length - extra;
     console.log(totalPages);
-    for(let i = 0; i < 6; i++) {
-        if(i == 0) {
-            box1.style.backgroundImage = `url(${currStore[0].image})`
-        }
-        if(i == 1) {
-            box2.style.backgroundImage = `url(${currStore[1].image})`
-        }
-        if(i == 2) {
-            box3.style.backgroundImage = `url(${currStore[2].image})`
-        }
-        if(i == 3) {
-            box4.style.backgroundImage = `url(${currStore[3].image})`
-        }
-        if(i == 4) {
-            box5.style.backgroundImage = `url(${currStore[4].image})`
-        }
-        if(i == 5) {
-            box6.style.backgroundImage = `url(${currStore[5].image})`
-        }
-    }
+    setPage();
+    
 }
 
 loadPage();
+
+
+
+box1.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[0 + pageIndex].url}`)
+    }
+})
+box2.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[1 + pageIndex].url}`)
+    }
+})
+box3.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[2 + pageIndex].url}`)
+    }
+})
+box4.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[3 + pageIndex].url}`)
+    }
+})
+box5.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[4 + pageIndex].url}`)
+    }
+})
+box6.addEventListener("click", function() {
+    if(currStore.length > 0) {
+        window.open(`${currStore[5 + pageIndex].url}`)
+    }
+})
